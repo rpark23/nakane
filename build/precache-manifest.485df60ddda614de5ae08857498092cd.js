b328ed94e2d3d5ef7727d5449cd5a8c08e24a1df
rpark23 <ryanparkusa@yahoo.com>
2020-09-01 11:56:32 -1000

    first commit

---
+++ build/precache-manifest.485df60ddda614de5ae08857498092cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3ec2536810e10b949cdd524b912ac2",
+    "url": "/kane-uhcc/index.html"
+  },
+  {
+    "revision": "6a15f8313fbb66a190c1",
+    "url": "/kane-uhcc/static/css/main.3f9cfb5c.chunk.css"
+  },
+  {
+    "revision": "957ea1ae77b0816caf65",
+    "url": "/kane-uhcc/static/js/2.f8aa90de.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/kane-uhcc/static/js/2.f8aa90de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a15f8313fbb66a190c1",
+    "url": "/kane-uhcc/static/js/main.c54e3558.chunk.js"
+  },
+  {
+    "revision": "d17ff4583d2433fd246e",
+    "url": "/kane-uhcc/static/js/runtime-main.17ea1988.js"
+  }
+]);
